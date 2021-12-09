c8ec115cef02b1e2f60c8dbc579f8acce0213ea4
Rene Schwartz <38519977+ReneJSchwartz@users.noreply.github.com>
2021-12-09 19:46:33 +0200

    lanternfish counting is possible for 256 days

--- Day6Lanternfish.cs
+++ Day6Lanternfish.cs
@@ -4,20 +4,69 @@ using System.Linq;
 using Sirenix.OdinInspector;
 
 [Button]
-void Start()
+void Start(int paramDays)
 {
     System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
     stopwatch.Start();
 
-    print($"method {nameof(AdventOfCode06Lanternfish)} returns {AdventOfCode06Lanternfish(days: 80)} as answer");
+
+    print($"method {nameof(LanternfishByDayPooling)} returns {LanternfishByDayPooling(days: paramDays)} as answer");
 
     stopwatch.Stop();
 
-    print($"operation took {stopwatch.ElapsedMilliseconds} milliseconds"); 
+    print($"operation took {stopwatch.Elapsed}"); 
+}
+
+// runs in about 1ms in unity and doesn't use mem much
+string LanternfishByDayPooling(int days)
+{
+    var path = Application.dataPath + "/AdventOfCode/06.txt";
+    string data = System.IO.File.ReadAllText(path);
+
+    string[] fishListStr = data.Split(',');
+    ulong[] fish = Array.ConvertAll(fishListStr, s => ulong.Parse(s));
+    ulong[] fishArr = new ulong[10];
+
+    ulong totalFishCount = 0;
+    
+    // populate starting fishes
+    for (int i = 0; i < fish.Length; i++)
+    {
+        fishArr[fish[i]]++;
+    }
+    // get starting fishcount
+    for (int i = 0; i < fishArr.Length; i++)
+    {
+        totalFishCount += fishArr[i];
+    }
+
+    for (int i = 0; i < days; i++)
+    {
+        // new fish addition p1
+        var newFishesAmt = fishArr[0];
+        totalFishCount += newFishesAmt;
+
+        fishArr[0] = 0;
+        fishArr[7] += newFishesAmt;
+
+        // move array 
+        for (int j = 1; j < 9 ; j++)
+        {
+            // all fishes are put 1 step closer to 0
+            fishArr[j-1] += fishArr[j];
+            fishArr[j] = 0;
+        }
+
+        // new fish addition p2
+        fishArr[8] += newFishesAmt;
+    }
+
+
+    return totalFishCount.ToString();
 }
 
 
-// runtime for first problem is ~29 ms
+// runtime for first problem is ~29 ms using arr slot for each fish ":D"
 // https://adventofcode.com/2021/day/6
 string AdventOfCode06Lanternfish(bool firstPart = true, int days = 0)
 {
